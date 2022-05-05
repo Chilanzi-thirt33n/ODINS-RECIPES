dd02ab5a1394953635f8a1f38c78c9922b473406
David sensenta <Chilanzi11037@gmail.com>
2022-05-05 12:57:14 +0200

    need to fix script bug

---
+++ scripts/recipes.js
@@ -0,0 +1,42 @@
+//**var for date that will be used by functions
+var myDate = new Date();
+let year = myDate.getFullYear();
+let today = myDate.getHours();
+
+//* MyMethod this is the function for changing html href 
+var RandomLink = function() {
+    var myLink;
+    if (today >= 6) {
+        myLink = 'Recipes/Pancakes.html';
+    } else if (today >= 12) {
+        myLink = 'Recipes/Nshima.html';
+    } else if (today >= 18) {
+        myLink = 'Recipes/Chicken.html'
+    } else {
+        myLink = 'Recipes/Chicken.html';
+    }
+    return myLink;
+};
+//* MyMethod for welcome
+var welcomeMsg = function() {
+    var message;
+    if (today >= 6) {
+        message = 'welcome and good morning its tea time';
+    } else if (today >= 12) {
+        message = "hello its lunch time grab something heavy";
+    } else if (today >= 18) {
+        message = 'evening we recommend something light at night';
+    } else {
+        message = 'welcome to food central';
+    }
+    return message;
+};
+//* MyMethod footer
+var myFooter = function() {
+    let message = '<p id="copyright">copyright &copy; arbit-tech  ' + year + ' Chilaaanzi</P>';
+    return message;
+};
+//* this is document section
+document.getElementById("welcome-message").innerHTML = welcomeMsg();
+document.getElementById("changeme").href = RandomLink();
+document.getElementById("copyright").innerHTML = myFooter();
